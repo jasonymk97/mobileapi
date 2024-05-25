4e22ff38e3725c9008719b548f4fb5c583901c21
jasonymk97 <jasonymk97@gmail.com>
2024-05-25 14:07:47 +0800

    Handle app.js

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
+// use dotenv
+require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const {verifyToken} = require('./middleware/jwtFilter');
+
+// Swagger
+const swaggerUI = require("swagger-ui-express");
+const swaggerDocument = require("./docs/openapi.json");
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth'); 
+const transactionsRouter = require('./routes/transaction');
+
+// configuration for database
+const db = require('./config/db');
 
 const app = express();
 
@@ -19,8 +31,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Define public paths that do not require authentication
+const publicPaths = ['/auth', '/docs'];
+
+// Apply JWT verification middleware globally, except for public paths
+app.use((req, res, next) => {
+  for (const path of publicPaths) {
+    if (req.path.startsWith(path)) {
+      return next(); // Skip token verification for paths starting with public paths
+    }
+  }
+  verifyToken(req, res, next);
+});
+
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/', indexRouter);
+app.use('/auth', authRouter);
 app.use('/users', usersRouter);
+app.use('/transactions', transactionsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
